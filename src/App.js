076371a3a8dc17c0c16fe26e97d369dd78ec003c
Nathik <nathik.zaman@ternarytech.co.uk>
2022-12-03 16:12:31 +0000

    Undoing test commiwwwt

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App(){
 
     return (
         <div className="container">
-            <h1>My App</h1>             
+            <h1>My App tese</h1>             
         </div>       
     )
 }
